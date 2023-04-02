6c11c2d9342f4d19802e370bbd42cf1d3d2f4b4d
Qthulhu42 <34537274+Qthulhu42@users.noreply.github.com>
2023-04-02 13:15:49 +1000

    Added functions to compute various system statistics

--- make_sim.py
+++ make_sim.py
@@ -1,9 +1,31 @@
 import numpy as np
 
-simple_exponential_queue(arrival_rate, service_rate, num_arrivals):
-    arrival_intervals = np.random.exponential(1/arrival_rate, num_arrivals)
-    service_intervals = np.random.exponential(1/service_rate, num_arrivals)
+def simple_exponential_queue(arrival_rate, service_rate, num_events_upper_bound = 10000):
+    events = np.cumsum(np.random.exponential(1/(arrival_rate + service_rate), num_events_upper_bound))
+    probabilities = np.array([service_rate,arrival_rate])
+    probabilities /= np.sum(probabilities)
+    deltas = np.random.choice([-1,1],p=probabilities,replace=True,size=num_events_upper_bound)
 
-    arrival_times, service_times = np.cumsum(arrival_intervals), np.cumsum(service_intervals)
-    
-    return arrival_times, service_times
+    n = 0
+    for i,delta in enumerate(deltas):
+        if n + delta < 0:
+            deltas[i] = 0
+        else:
+            n = n + delta
+
+    valid_events = (deltas != 0)
+
+    return events[valid_events],deltas[valid_events]
+
+def utilisation_rate(events, deltas):
+    return sum(np.diff(events) * (np.cumsum(deltas)[:-1] > 0)) / events[-1]
+
+def mean_waiting_time(events, deltas):
+    arrivals = events[deltas>0]
+    departures = events[deltas<0]
+    ad = zip(arrivals,departures)
+    w = np.array([t[1]-t[0] for t in ad])
+    return np.mean(w)
+
+def mean_queue_len(events, deltas):
+    return sum(np.diff(events) * np.cumsum(deltas)[:-1]) / events[-1]
